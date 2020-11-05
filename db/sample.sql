f8f8fe65e9537a5d6e93afc8439cbf09794f4e3c
Ryan Xin <xinpf0715@gmail.com>
2020-11-05 20:50:38 +1100

    update sample.sql

--- db/sample.sql
+++ db/sample.sql
@@ -7,11 +7,10 @@
 -- Server version: 5.7.26
 -- PHP Version: 7.2.19
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
-
+SET time_zone = '+00:00';
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,50 +18,191 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `database`
+-- Database: 'database'
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sample`
+-- Table structure for table 'keyword'
 --
 
-CREATE TABLE `sample` (
-  `id` int(255) NOT NULL,
-  `name` varchar(100) NOT NULL
+CREATE TABLE 'keyword' (
+  'keyword_id' int(255) NOT NULL,
+  'data' varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `sample`
+-- Dumping data for table 'keyword'
 --
 
-INSERT INTO `sample` (`id`, `name`) VALUES
-(1, 'andrew'),
-(2, 'brian'),
-(3, 'charles'),
-(4, 'david');
+INSERT INTO keyword ('keyword_id', 'data') VALUES
+(1, 'Shoes'),
+(2, 'Shoes carnival'),
+(3, 'Shoes palace'),
+(4, 'Shoes stores near me'),
+(5, 'Shoes for crew'),
+(6, 'Shoes station'),
+(7, 'Shoes repair'),
+(8, 'Shoes store'),
+(9, 'Shoes for women'),
+(10, 'Shoes fashion week'),
+(11, 'Shoes instagram'),
+(12, 'Shoes stories'),
+(13, 'Shoes show');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `sample`
+-- Indexes for table 'keyword'
 --
-ALTER TABLE `sample`
-  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE 'keyword'
+ADD PRIMARY KEY ('keyword_id');
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `sample`
+-- AUTO_INCREMENT for table 'keyword'
 --
-ALTER TABLE `sample`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
+
+ALTER TABLE 'keyword'
+  MODIFY 'keyword_id' int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table 'site'
+--
+
+CREATE TABLE 'site' (
+  'site_id' int(255) NOT NULL,
+  'data' varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table 'site'
+--
+
+INSERT INTO sites ('sie_id', 'data') VALUES
+(1, 'www.dockers.com'),
+(2, 'www.adidas.com'),
+(3, 'www.nike.com'),
+(4, 'www.underarmour.com'),
+(5, 'www.newbalance.com'),
+(6, 'www.puma.com'),
+(7, 'www.prada.com'),
+(8, 'www.fredperry.com'),
+(9, 'www.caterpillar.com'),
+(10, 'www.gucci.com'),
+(11, 'www.allenedmonds.com'),
+(12, 'www.brunomagli.com'),
+(13, 'www.diesel.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table 'site'
+--
+
+ALTER TABLE 'site'
+ADD PRIMARY KEY ('site_id');
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table 'keyword'
+--
+
+ALTER TABLE 'site'
+  MODIFY 'site_id' int
+(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table 'setting'
+--
+
+CREATE TABLE 'setting' (
+  'setting_id' int(255) NOT NULL,
+  'data' JSON
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table 'setting'
+--
+
+INSERT INTO settings ('setting_id', 'data') VALUES
+(1, '{
+  "browsers": {
+    "chrome": false,
+    "firefox": false,
+    "explorer": true,
+    "safari": false,
+    "opera": false
+  },
+  "incognito": true,
+  "inputs": {
+    "wait_seconds_1": 40,
+    "wait_seconds_2": 55,
+    "visit_within_site": true,
+    "page_numbers": 1,
+    "page_visit_seconds_1": 30,
+    "page_visit_seconds_2": 50,
+    "after_operation_wait_seconds_1": 5,
+    "after_operation_wait_seconds_2": 10,
+    "target_sites": 10,
+    "target_site_wait_minutes": 20,
+    "auto_reset_times": 1
+  },
+  "devices": {
+    "device_reset": false,
+    "vinn_reset": false,
+    "phone_reset": true,
+    "mobile_data": true,
+    "fly_mode": false
+  },
+  "options": {
+    "remove_cookies": true,
+    "change_resolution": false,
+    "mouse_tracks": false,
+    "data_saving_mode": true,
+    "random_generate": false,
+    "analytics_protection": true,
+    "remove_history": false
+  }
+}');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table 'keyword'
+--
+
+ALTER TABLE 'setting'
+ADD PRIMARY KEY ('setting_id');
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table 'setting'
+--
+
+ALTER TABLE 'setting'
+  MODIFY 'setting_id' int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
